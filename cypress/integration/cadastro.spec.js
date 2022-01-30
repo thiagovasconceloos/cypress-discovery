6f8ebc6b1575beb4bf7d40fa4417255c5b608a88
Thiago Vasconcelos <vasconcelosthiago@outlook.com>
2022-01-30 18:27:45 -0300

    Combinando CSS Selector com Texto

--- cypress/integration/cadastro.spec.js
+++ cypress/integration/cadastro.spec.js
@@ -1,5 +1,5 @@
 // criar arquivo .gitignore para não commitar a pasta node_modules
-// no windows é echo >.gitignore node_modules
+// usar o site gitignore.io para criar o gitignore (+ facil)
 
 describe('Cadastro',()=>{ 
 
@@ -13,16 +13,58 @@ describe('Cadastro',()=>{
     cy.get('a[href="/deliver"]').click();
     // checkpoint para saber se estamos no lugar certo
     cy.get('#page-deliver form h1').should('have.text','Cadastre-se para  fazer entregas')
-
+    
      
+    //aqui temos um objeto dentro de outro objeto.
      const entregador = {
        nome : 'Thiago Vasconcelos ',
        cpf : '70070070070',
        email : 'Thiago@email.com',
-       whatsapp: '11999999999'
+       whatsapp: '11999999999',
+       endereco: {
+         cep: '04534011',
+         rua: 'Rua Joaquim Floriano',
+         numero: '1000',
+         complemento: 'Ap 145',
+         bairro: 'Itaim Bibi',
+         cidade_uf:'São Paulo/SP'
+
+
+       },
+       metodo_entrega : 'Moto'
 
 
      }
+
+    // input nome a partir do objeto 
+    cy.get('input[name="name"]').type(entregador.nome)
+    // input cpf a partir do objeto
+    cy.get('input[name="cpf"]').type(entregador.cpf)
+    // input email a partir do objeto
+    cy.get('input[name="email"]').type(entregador.email)
+    // input whatsapp a partir do objeto
+    cy.get('input[name="whatsapp"]').type(entregador.whatsapp)
+    // input cep a partir do objeto
+    cy.get('input[name="postalcode"]').type(entregador.endereco.cep)
+    // clicando no botão buscar cep, 
+    //colocamos duas propriedades para garantir 
+    //que vai clicar no lugar certo.
+    cy.get('input[type="button"][value="Buscar CEP"').click();
+    // input numero a partir do objeto
+    cy.get('input[name="address-number"]').type(entregador.endereco.numero)
+    // input complemento a partir do objeto
+    cy.get('input[name="address-details"]').type(entregador.endereco.complemento)
+
+    // validando o valor do campo address com o valor do campo rua no objeto endereco 
+    cy.get('input[name="address"]').should('have.value',entregador.endereco.rua)
+    // validando o valor do campo district com o valor do campo bairro no objeto  endereco 
+    cy.get('input[name="district"]').should('have.value',entregador.endereco.bairro)
+    // validando o valor do campo uf com o valor do  campo cidade-uf no objeto endereco 
+    cy.get('input[name="city-uf"]').should('have.value',entregador.endereco.cidade_uf)
+    // clicando na opção moto 
+    //usamos o contains por se tratar de uma lista não temos propriedades
+    // acessiveis, sendo assim estamos buscando o valor que existe em cada li
+    cy.contains('.delivery-method li',entregador.metodo_entrega).click();
 
 
    })
