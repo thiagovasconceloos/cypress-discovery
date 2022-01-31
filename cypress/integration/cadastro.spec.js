444b71f565947c47995333eeeae652e3d53065a0
Thiago Vasconcelos <vasconcelosthiago@outlook.com>
2022-01-31 13:09:25 -0300

    Page Objects

--- cypress/integration/cadastro.spec.js
+++ cypress/integration/cadastro.spec.js
@@ -1,93 +1,61 @@
-// criar arquivo .gitignore para não commitar a pasta node_modules
-// usar o site gitignore.io para criar o gitignore (+ facil)
+import SignupPage from '../pages/SignupPage'
 
-describe('Cadastro',()=>{ 
+describe('Cadastro', () => {
 
-   it('Usuario deve se tornar um entregador',()=>{
+    it('Usuário deve se tornar um entregador', () => {
 
-    // função viewport redimensiona a pagina para um tamanho padrão
-    cy.viewport(1440,900)
-    // funcao visit, como o nome sugere visita a pagina mencionada
-    cy.visit('https://buger-eats.vercel.app/')
-    // funcao get localiza a rota e clica 
-    cy.get('a[href="/deliver"]').click();
-    // checkpoint para saber se estamos no lugar certo
-    cy.get('#page-deliver form h1').should('have.text','Cadastre-se para  fazer entregas')
-    
-     
-    //aqui temos um objeto dentro de outro objeto.
-     const entregador = {
-       nome : 'Thiago Vasconcelos ',
-       cpf : '70070070070',
-       email : 'Thiago@email.com',
-       whatsapp: '11999999999',
-       endereco: {
-         cep: '04534011',
-         rua: 'Rua Joaquim Floriano',
-         numero: '1000',
-         complemento: 'Ap 145',
-         bairro: 'Itaim Bibi',
-         cidade_uf:'São Paulo/SP'
+        var deliver = {
+            name: 'Thiago Vasconcelos',
+            cpf: '00000014141',
+            email: 'thiago@email.com',
+            whatsapp: '11999999999',
+            address: {
+                postalcode: '04534011',
+                street: 'Rua Joaquim Floriano',
+                number: '1000',
+                details: 'Ap 142',
+                district: 'Itaim Bibi',
+                city_state: 'São Paulo/SP'
+            },
+            delivery_method: 'Moto',
+            cnh: 'images/cnh-digital.jpg'
+        }
 
+        var signup = new SignupPage()
 
-       },
-       metodo_entrega : 'Moto',
-       cnh:'images/cnh-digital.jpg'
+        signup.go()
+        signup.fillForm(deliver)
+        signup.submit()
 
+        const expectedMessage = 'Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.'
+        signup.modalContentShouldBe(expectedMessage)
+    })
 
-     }
+    it('CPF incorreto', () => {
 
-    // input nome a partir do objeto 
-    cy.get('input[name="name"]').type(entregador.nome)
-    // input cpf a partir do objeto
-    cy.get('input[name="cpf"]').type(entregador.cpf)
-    // input email a partir do objeto
-    cy.get('input[name="email"]').type(entregador.email)
-    // input whatsapp a partir do objeto
-    cy.get('input[name="whatsapp"]').type(entregador.whatsapp)
-    // input cep a partir do objeto
-    cy.get('input[name="postalcode"]').type(entregador.endereco.cep)
-    // clicando no botão buscar cep, 
-    //colocamos duas propriedades para garantir 
-    //que vai clicar no lugar certo.
-    cy.get('input[type="button"][value="Buscar CEP"').click();
-    // input numero a partir do objeto
-    cy.get('input[name="address-number"]').type(entregador.endereco.numero)
-    // input complemento a partir do objeto
-    cy.get('input[name="address-details"]').type(entregador.endereco.complemento)
+        var deliver = {
+            name: 'Thiago Vasconcelos',
+            cpf: '000000141AA',
+            email: 'thiago@email',
+            whatsapp: '11999999999',
+            address: {
+                postalcode: '04534011',
+                street: 'Rua Joaquim Floriano',
+                number: '1000',
+                details: 'Ap 142',
+                district: 'Itaim Bibi',
+                city_state: 'São Paulo/SP'
+            },
+            delivery_method: 'Moto',
+            cnh: 'images/cnh-digital.jpg'
+        }
 
-    // validando o valor do campo address com o valor do campo rua no objeto endereco 
-    cy.get('input[name="address"]').should('have.value',entregador.endereco.rua)
-    // validando o valor do campo district com o valor do campo bairro no objeto  endereco 
-    cy.get('input[name="district"]').should('have.value',entregador.endereco.bairro)
-    // validando o valor do campo uf com o valor do  campo cidade-uf no objeto endereco 
-    cy.get('input[name="city-uf"]').should('have.value',entregador.endereco.cidade_uf)
-    // clicando na opção moto 
-    //usamos o contains por se tratar de uma lista não temos propriedades
-    // acessiveis, sendo assim estamos buscando o valor que existe em cada li
-    cy.contains('.delivery-method li',entregador.metodo_entrega).click();
-    // expressões regulares basicas:
-    // ^ buscar prefixo 
-    // $ buscar sufixo
-    // * buscar geral
-    // upload do arquivo que esta na subpasta images dentro de fixtures
-    cy.get('input[accept^="image"]').attachFile(entregador.cnh)
-    // clica no bottão submit para finalizar o cadastro
-    cy.get('form button[type="submit"]').click()
-    const expectedMessage = 'Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.'
+        var signup = new SignupPage()
 
-    // valida se o cadastro foi concluido.
-    cy.get('.swal2-container .swal2-html-container')
-    .should('have.text',expectedMessage)
+        signup.go()
+        signup.fillForm(deliver)
+        signup.submit()
+        signup.alertMessageShouldBe('Oops! CPF inválido')
+    })
 
-   })
-
-
-
-
-    
 })
-
-
-
-
---
+++ cypress/pages/SignupPage.js
@@ -0,0 +1,88 @@
+class SignupPage {
+
+ go(){
+
+  
+    // função viewport redimensiona a pagina para um tamanho padrão
+    cy.viewport(1440,900)
+    // funcao visit, como o nome sugere visita a pagina mencionada
+    cy.visit('https://buger-eats.vercel.app/')
+    // funcao get localiza a rota e clica 
+    cy.get('a[href="/deliver"]').click();
+    // checkpoint para saber se estamos no lugar certo
+    cy.get('#page-deliver form h1').should('have.text','Cadastre-se para  fazer entregas')
+    
+
+
+ }
+
+ fillForm(deliver){
+
+      // input nome a partir do objeto 
+      cy.get('input[name="name"]').type(deliver.name)
+    // input cpf a partir do objeto
+    cy.get('input[name="cpf"]').type(deliver.cpf)
+    // input email a partir do objeto
+    cy.get('input[name="email"]').type(deliver.email)
+    // input whatsapp a partir do objeto
+    cy.get('input[name="whatsapp"]').type(deliver.whatsapp)
+    // input cep a partir do objeto
+    cy.get('input[name="postalcode"]').type(deliver.address.postalcode)
+    // clicando no botão buscar cep, 
+    //colocamos duas propriedades para garantir 
+    //que vai clicar no lugar certo.
+    cy.get('input[type="button"][value="Buscar CEP"').click();
+    // input numero a partir do objeto
+    cy.get('input[name="address-number"]').type(deliver.address.number)
+    // input complemento a partir do objeto
+    cy.get('input[name="address-details"]').type(deliver.address.details)
+
+    // validando o valor do campo address com o valor do campo rua no objeto endereco 
+    cy.get('input[name="address"]').should('have.value',deliver.address.street)
+    // validando o valor do campo district com o valor do campo bairro no objeto  endereco 
+    cy.get('input[name="district"]').should('have.value',deliver.address.district)
+    // validando o valor do campo uf com o valor do  campo cidade-uf no objeto endereco 
+    cy.get('input[name="city-uf"]').should('have.value',deliver.address.city_state)
+    // clicando na opção moto 
+    //usamos o contains por se tratar de uma lista não temos propriedades
+    // acessiveis, sendo assim estamos buscando o valor que existe em cada li
+    cy.contains('.delivery-method li',deliver.delivery_method).click();
+    // expressões regulares basicas:
+    // ^ buscar prefixo 
+    // $ buscar sufixo
+    // * buscar geral
+    // upload do arquivo que esta na subpasta images dentro de fixtures
+    cy.get('input[accept^="image"]').attachFile(deliver.cnh)
+    // clica no bottão submit para finalizar o cadastro
+
+ }
+
+  submit(){
+
+    cy.get('form button[type="submit"]').click()
+
+
+
+  }
+ 
+
+  modalContentShouldBe(expectedMessage){
+
+    // valida se o cadastro foi concluido.
+    cy.get('.swal2-container .swal2-html-container')
+    .should('have.text',expectedMessage)
+
+
+
+  }
+
+  alertMessageShouldBe(expectedMessage) {
+
+    // valida se teve algum erro
+    cy.get('.alert-error').should('have.text', expectedMessage)
+}
+
+
+} 
+
+ export default SignupPage
